64d7f8f41d86a37c49c4f5198d5e4cf6820b8574
Cory Steers <cory.steers.gmu9@statefarm.com>
2017-12-19 17:17:16 -0600

    initial coding through initial viewcontroler fetch

---
+++ Project38/Project38/ViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  Project38
+//
+//  Created by Cory Steers on 12/19/17.
+//  Copyright © 2017 Cory Steers. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UITableViewController {
+
+    private var container: NSPersistentContainer!
+    
+    private var commits = [Commit]()
+    
+    private func saveContext() {
+        if container.viewContext.hasChanges {
+            do {
+                try container.viewContext.save()
+            } catch {
+                print("An error occurred while saving: \(error)")
+            } }
+    }
+    
+    @objc private func fetchCommits() {
+        if let data = try? String(contentsOf: URL(string: "https://api.github.com/repos/apple/swift/commits?per_page=100")!) {
+            let jsonCommits = JSON(parseJSON: data)
+            let jsonCommitArray = jsonCommits.arrayValue
+            print("Received \(jsonCommitArray.count) new commits.")
+            DispatchQueue.main.async { [unowned self] in
+                for jsonCommit in jsonCommitArray {
+                    let commit = Commit(context: self.container.viewContext)
+                    self.configure(commit, usingJSON: jsonCommit)
+                }
+                self.saveContext()
+                self.loadSavedData()
+            }
+        }
+        
+    }
+    
+    private  func configure(_ commit: Commit, usingJSON json: JSON) {
+        commit.sha = json["sha"].stringValue
+        commit.message = json["commit"]["message"].stringValue
+        commit.url = json["html_url"].stringValue
+        let formatter = ISO8601DateFormatter()
+        commit.date = formatter.date(from: json["commit"]["committer"]["date"].stringValue) ?? Date()
+    }
+    
+    func loadSavedData() {
+        let request: NSFetchRequest<Commit> = Commit.fetchRequest()
+        let sort = NSSortDescriptor(key: "date", ascending: false)
+        request.sortDescriptors = [sort]
+        do {
+            commits = try container.viewContext.fetch(request)
+            print("Got \(commits.count) commits")
+            tableView.reloadData()
+        } catch {
+            print("Fetch failed")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        container = NSPersistentContainer(name: "Project38")
+        
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                print("Unresolved error \(error)")
+            }
+        }
+        
+//        let commit = Commit()
+        let commit = Commit.init(entity: NSEntityDescription.entity(forEntityName: "Commit", in: container.viewContext)!, insertInto: container.viewContext)
+        commit.message = "Woo"
+        commit.url = "http://www.example.com"
+        
+        performSelector(inBackground: #selector(fetchCommits), with: nil)
+        
+        loadSavedData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func numberOfSections(in tableView: UITableView) ->
+        Int {
+            return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commits.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt
+        indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:
+            "Commit", for: indexPath)
+        let commit = commits[indexPath.row]
+        cell.textLabel!.text = commit.message
+        cell.detailTextLabel!.text = commit.date?.description ?? "N/A"
+        return cell
+    }
+}
+
