a7d2e9d30fd107887b1f0c714d2c95d38d9aee3c
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-01-09 16:04:09 -0600

    working through page 702

---
+++ Multibrowser/Multibrowser/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Multibrowser
+//
+//  Created by Cory Steers on 1/9/18.
+//  Copyright © 2018 Cory Steers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate {
+
+    @IBOutlet weak var addressBar: UITextField!
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    private func setDefaultTitle() {
+        title = "Multibrowser"
+    }
+    
+    @objc func addWebView() {
+        let webView = UIWebView()
+        webView.delegate = self
+        stackView.addArrangedSubview(webView)
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        
+//        DispatchQueue.global(qos: .userInitiated).async {
+            webView.loadRequest(URLRequest(url: url))
+            /*
+            DispatchQueue.main.async {
+                // anything needed here?
+            }
+            */
+//        }
+    }
+    
+    @objc func deleteWebView() {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDefaultTitle()
+        let add = UIBarButtonItem(barButtonSystemItem: .add, target:
+            self, action: #selector(addWebView))
+        let delete = UIBarButtonItem(barButtonSystemItem: .trash,
+                                     target: self, action: #selector(deleteWebView))
+        navigationItem.rightBarButtonItems = [delete, add]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
