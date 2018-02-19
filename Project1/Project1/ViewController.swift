a1bcfe7bbbae9f227af8be9d3c632a8a91e2f15a
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-02-19 16:12:45 -0600

    initial coding

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Cory Steers on 2/19/18.
+//  Copyright © 2018 Cory Steers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
