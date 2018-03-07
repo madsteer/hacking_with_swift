09643690f20072bae7167170a78ac8871f2b5072
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-03-07 16:19:44 -0600

    interface is bult

---
+++ Project08/Project08/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Project08
+//
+//  Created by Cory Steers on 3/7/18.
+//  Copyright © 2018 Cory Steers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var cluesLabel: UILabel!
+    @IBOutlet var answersLabel: UILabel!
+    @IBOutlet var currentAnswer: UITextField!
+    @IBOutlet var scoreLabel: UILabel!
+
+
+    @IBAction func submitTapped(_ sender: Any) {
+    }
+    
+    @IBAction func clearTapped(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
