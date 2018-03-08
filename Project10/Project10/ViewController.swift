49fd865dbea59bb9ce5624661470e146c5035e11
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-03-08 15:58:17 -0600

    initial interface built

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Cory Steers on 3/8/18.
+//  Copyright © 2018 Cory Steers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
