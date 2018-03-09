b502d4e644a530c7dc1295b1f774c298540fa576
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-03-09 10:40:43 -0600

    finished image collection view app (project 10)

--- Project10/Project10/Person.swift
+++ Project10/Project10/Person.swift
@@ -10,4 +10,11 @@ import UIKit
 
 class Person: NSObject {
 
+    var name: String
+    var image: String
+
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
 }
