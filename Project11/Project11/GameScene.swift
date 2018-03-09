6a6ef53772e86e218e249958a5868f0cfb057af2
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-03-09 15:14:00 -0600

    added collision detection and removing ball once it hits a slot

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -8,7 +8,7 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
 
 
     override func didMove(to view: SKView) {
@@ -17,7 +17,9 @@ class GameScene: SKScene {
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsWorld.contactDelegate = self
 
         for x in [0, 256, 512, 768, 1024] {
             makeBouncer(at: CGPoint(x: x, y: 0))
@@ -44,13 +46,18 @@ class GameScene: SKScene {
         case true:
             slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"
         case false:
             slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"
         }
 
         slotBase.position = position
         slotGlow.position = position
+
+        slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
+        slotBase.physicsBody?.isDynamic = false
 
         addChild(slotBase)
         addChild(slotGlow)
@@ -59,14 +66,41 @@ class GameScene: SKScene {
         let spinForever = SKAction.repeatForever(spin)
         slotGlow.run(spinForever)
     }
+
+    private func destroy(ball: SKNode) {
+        ball.removeFromParent()
+    }
+
+    private func collisionBetween(ball: SKNode, object: SKNode) {
+        if object.name == "good" {
+            destroy(ball: ball)
+        } else if object.name == "bad" {
+            destroy(ball: ball)
+        }
+    }
+
+    internal func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+
+        if nodeA.name == "ball" {
+            collisionBetween(ball: contact.bodyA.node!, object: contact.bodyB.node!)
+        } else if nodeB.name == "ball" {
+            collisionBetween(ball: contact.bodyB.node!, object: contact.bodyA.node!)
+        }
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let location = touch.location(in: self)
             let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.name = "ball"
+
             ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+            ball.physicsBody?.contactTestBitMask = ball.physicsBody!.collisionBitMask
             ball.physicsBody?.restitution = 0.4
             ball.position = location
+
             addChild(ball)
         }
     }
