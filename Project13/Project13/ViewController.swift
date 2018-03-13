048739dcce8637289ec53b2d829c77e831504221
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-03-13 18:51:40 -0500

    coded for Sepia tone filtering

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import CoreImage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var imageView: UIImageView!
 
     @IBOutlet var intensity: UISlider!
+
+    var currentImage: UIImage!
+    var context: CIContext!
+    var currentFilter: CIFilter!
 
     @IBAction func changeFilter(_ sender: Any) {
     }
@@ -20,18 +25,45 @@ class ViewController: UIViewController {
     }
 
     @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target:
+            self, action: #selector(importPicture))
+
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc private func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
 
+        applyProcessing()
+    }
+
+    func applyProcessing() {
+        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        if let cgimg = context.createCGImage(currentFilter.outputImage!, from:
+                                             currentFilter.outputImage!.extent) {
+
+            let processedImage = UIImage(cgImage: cgimg)
+            imageView.image = processedImage
+        }
+    }
 }
 
