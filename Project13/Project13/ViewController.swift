1a82518ae6aad385a22dd973ad232f499dbf5704
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-03-13 15:33:43 -0500

    wired up interface to code

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
 
+    @IBOutlet var intensity: UISlider!
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+
+    @IBAction func save(_ sender: Any) {
+    }
+
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
