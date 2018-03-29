cfc25343ecc5ac1114697b38e8288c1fbe228176
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-03-29 11:11:01 -0500

    initial setup

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Cory Steers on 3/29/18.
+//  Copyright © 2018 Cory Steers. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+}
