a07056e14108e95b3c88f881c62ddf6cc0775d4f
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-04-03 10:47:58 -0500

    more coding.  Problem getting background to paint correctly

--- Project17/Project17/GameScene.swift
+++ Project17/Project17/GameScene.swift
@@ -11,14 +11,114 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        } }
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+    var activeSlicePoints = [CGPoint]()
 
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        createScore()
+        createLives()
+        createSlices()
+    }
+
+    private func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        gameScore.position = CGPoint(x: 8, y: 8)
+    }
+
+    private func createLives() {
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)),
+                                          y: 720)
+            addChild(spriteNode)
+            livesImages.append(spriteNode)
+        }
+    }
+
+    private func createSlices() {
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 2
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9,
+                                            blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+        activeSliceFG.strokeColor = UIColor.white
+        activeSliceFG.lineWidth = 5
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
+    }
+
+    private func redrawActiveSlice() {
+        if activeSlicePoints.count < 2 {
+            activeSliceBG.path = nil
+            activeSliceFG.path = nil
+            return
+        }
+
+        activeSlicePoints = Array(activeSlicePoints.suffix(12))
+//        while activeSlicePoints.count > 12 {
+//            activeSlicePoints.remove(at: 0)
+//        }
+
+        let path = UIBezierPath()
+        path.move(to: activeSlicePoints[0])
+        for i in 1 ..< activeSlicePoints.count {
+            path.addLine(to: activeSlicePoints[i])
+        }
+
+        activeSliceBG.path = path.cgPath
+        activeSliceFG.path = path.cgPath
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
 
+        activeSlicePoints.removeAll(keepingCapacity: true)
+
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            activeSlicePoints.append(location)
+            redrawActiveSlice()
+            activeSliceBG.removeAllActions()
+            activeSliceFG.removeAllActions()
+            activeSliceBG.alpha = 1
+            activeSliceFG.alpha = 1
+        }
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        activeSlicePoints.append(location)
+        redrawActiveSlice()
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        activeSliceBG.run(SKAction.fadeOut(withDuration: 0.25))
+        activeSliceFG.run(SKAction.fadeOut(withDuration: 0.25))
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touchesEnded(touches, with: event)
     }
 }
