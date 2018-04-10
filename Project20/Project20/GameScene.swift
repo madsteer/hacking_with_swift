4625f7bb74b60de4aade8854377179bd7be179b0
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-04-10 11:14:39 -0500

    initial project setup

---
+++ Project20/Project20/GameScene.swift
@@ -0,0 +1,37 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by Cory Steers on 4/10/18.
+//  Copyright © 2018 Cory Steers. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
