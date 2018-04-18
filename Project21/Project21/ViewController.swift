cadf19d3a2a0c990999960ae76957e36a828cc5a
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-04-18 13:29:26 -0500

    finished initial code to set an alert 1 minute from when the schedule button was clicked.  interval type was left in commented out

--- Project21/Project21/ViewController.swift
+++ Project21/Project21/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
@@ -19,9 +20,41 @@ class ViewController: UIViewController {
     }
 
     @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
     }
-    
+
     @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = UNNotificationSound.default()
+
+        if let date = Calendar.current.date(byAdding: .minute, value: 1, to: Date()) {
+
+            var dateComponents = DateComponents()
+            dateComponents.hour = Calendar.current.component(.hour, from: date)
+            dateComponents.minute = Calendar.current.component(.minute, from: date)
+            print("date hour: \(String(describing: dateComponents.hour)), date minute: \(String(describing: dateComponents.minute))")
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+
+            // do interval instead
+//            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 15, repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+            print("Scheduled!")
+        }
     }
 
 }
