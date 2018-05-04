5457cff81c26ff4549e7192bd15b3b5e31010b13
Cory Steers <cory.steers.gmu9@statefarm.com>
2018-05-04 12:25:05 -0500

    initial playground setup and plusOne Int Extension

---
+++ Project24/Project24.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+extension Int {
+    func plusOne() -> Int {
+        return self + 1
+    }
+}
+
+var myInt = 0
+myInt.plusOne()
+myInt
+
+5.plusOne()
